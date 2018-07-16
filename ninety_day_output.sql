f67c5527e4ddb0af475814ad2a04c66ac591894e
ray <ray@rvoelke-laptop.plch.net>
2018-07-16 11:32:16 -0400

    version 0.2 --added 90 day holds report

---
+++ ninety_day_output.sql
@@ -0,0 +1,21 @@
+﻿SELECT 
+t.bib_number as bib_num,
+t.vol as vol,
+t.pub_year as pub_year,
+t.cat_date as cat_date,
+t.media_type as media_type,
+t.title as title,
+t.call_number as call_number,
+t.over_90_not_os as over_90_not_os,
+t.over_90_os as over_90_os,
+t.count_active_holds as count_active_holds,
+t.count_active_copies as count_active_copies,
+t.count_copies_on_order as count_copies_on_order
+
+FROM 
+temp_90_day_output AS t
+
+ORDER BY
+t.bcode2,
+t.best_title_norm
+;
---
+++ test_queries/item_data.sql
@@ -0,0 +1,40 @@
+﻿-- SELECT
+-- reckey2id('b3254165')
+
+
+SELECT
+id2reckey(l.bib_record_id) as bib_num,
+id2reckey(l.item_record_id) as item_num,
+i.is_suppressed,
+i.item_status_code,
+c.checkout_gmt::date,
+c.due_gmt::date,
+r.record_last_updated_gmt::date
+-- c.*,
+-- i.*
+
+FROM
+sierra_view.bib_record_item_record_link as l
+
+JOIN
+sierra_view.item_record as i
+ON
+  i.record_id = l.item_record_id
+
+JOIN
+sierra_view.record_metadata as r
+ON
+  r.id = l.item_record_id
+
+LEFT OUTER JOIN
+sierra_view.checkout as c
+ON
+  c.item_record_id = l.item_record_id
+
+where
+l.bib_record_id = 420910049173
+
+order by
+i.is_suppressed DESC,
+item_status_code,
+c.due_gmt
